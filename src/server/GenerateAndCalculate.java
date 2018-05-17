7260cc8d754c2d090fe7dfdc9e59453f565ab45d
Elina <e.kuzmina11@gmail.com>
2018-05-17 14:59:30 +0300

    last commit

--- src/server/GenerateAndCalculate.java
+++ src/server/GenerateAndCalculate.java
@@ -5,10 +5,36 @@
  */
 package server;
 
+import java.util.ArrayList;
+import java.util.Random;
+import javasem04.activity.PersonList;
+import model.Candidate;
+
 /**
  *
  * @author Elina
  */
 public class GenerateAndCalculate {
+
+    static PersonList list = new PersonList();
+    static ArrayList<Candidate> candidates = new ArrayList();
+    Random rand = new Random();
+    static Candidate tmp;
+    int randomAge;
     
+    public GenerateAndCalculate() {
+    }
+    
+    public void generateCandidates() {
+        
+        for(int i = 0; i < list.getList().size() i++) {
+            randomAge = rand.nextInt(88)+18;
+            tmp = new Candidate(list.getList().get(i).getName(), randomAge, "");
+            candidates.add(tmp);
+        }
+    }
+    
+    public static void calculateResults() {
+        /*...*/
+    }
 }
